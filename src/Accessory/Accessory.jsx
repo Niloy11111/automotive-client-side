7ed400cc523ed32483940f6f772dcd242a1f54a2
Niloy11111 <Ektiaruddinniloy859@gmail.com>
2023-10-21 22:25:14 +0600

    changed website title to automotive assignment

--- src/Accessory/Accessory.jsx
+++ src/Accessory/Accessory.jsx
@@ -11,10 +11,10 @@ const Accessory = () => {
         }}>
         
         <div className="ml-20 pt-20">
-        <h2 className="text-[#FFF] text-3xl">THW BMW <br />
+        <h2 className="text-[#FFF] text-5xl">THW BMW <br />
         ACCESSORY CENTER</h2>
         <h3 className="mt-7 text-xl text-[#FFF]">Customize your SUV before delivery</h3>
-        <NavLink to="/login"><button className="px-6 lg:px-12 text-[#221f1f] py-3 lg:py-4 text-sm font-Inter mt-6 rounded bg-[#FFF]">Learn More</button> </NavLink>
+        <NavLink to="/login"><button className="px-6 lg:px-16 text-[black] font-extrabold py-3 lg:py-4 text-lg font-Inter mt-6 bg-[#FFF]">Learn More</button> </NavLink>
         </div>
         
         </div>
