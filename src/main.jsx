0a1a972c8090091bc05f20266d29f3d4593403ea
Niloy11111 <Ektiaruddinniloy859@gmail.com>
2024-02-22 21:11:07 +0600

    edited

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import Favicon from "react-favicon";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className=" bg-[#FAFEEC] text-[#0B0E01] dark:bg-[#191919] font-Inter">
+  <div className=" bg-[#FAFEEC] text-[#0B0E01] dark:bg-[#191919] font-Inter min-h-screen">
     <Favicon url="https://i.ibb.co/47ZF47v/girl-with-red-vintage-convertible-car-1308-111836.jpg" />
     <React.StrictMode>
       <AuthProvider>
